3ef4a830feee46690d2de16d59550e97fb00193d
Daegas <darae.gs@gmail.com>
2019-12-03 17:32:54 -0600

    SeedDB-Tutorial 57 parte6

---
+++ Clinica.Web/Data/SeedDB.cs
@@ -0,0 +1,42 @@
+﻿namespace Clinica.Web.Data
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Clinica.Web.Data.Entities;
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Pacients.Any())
+            {
+                this.AddProduct("Pedro Perez");
+                this.AddProduct("Diana García");
+                this.AddProduct("Miguel Rodriguez");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            _ = this.context.Pacients.Add(new Pacients
+            {
+                Name = name,
+                Address = "Una calle 20"
+            });
+        }
+    }
+
+}
--- Clinica.Web/Program.cs
+++ Clinica.Web/Program.cs
@@ -10,15 +10,33 @@ using Microsoft.Extensions.Logging;
 
 namespace Clinica.Web
 {
+    using Data;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
+        }
+
+        private static void RunSeeding(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
+
 }
